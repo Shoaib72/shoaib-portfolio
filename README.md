9210b229e9459989b243457543fb4b5bdf505176
Shoaib72 <shoaib.953872@gmail.com>
2022-12-11 05:43:19 +0600

    updated

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Live site link: https://aesthetic-arithmetic-6406a5.netlify.app/home
+# Live site link: https://aesthetic-arithmetic-6406a5.netlify.app/
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -8,33 +8,33 @@ const Projects = () => {
         <div>
             <h2 className="text-3xl text-red-600 font-bold text-center absolute top-[150px] md:ml-[600px]">MY PROJECTS</h2>
             <div className='absolute top-[200px] flex md:gap-[40px] gap-[22px] md:ml-[260px]'>
-                <div className="card md:w-64 w-36 bg-base-100 shadow-xl">
+                <div className="card md:w-64 w-36 bg-black shadow-xl">
                     <figure><img src={P1} alt="Shoes" /></figure>
                     <div className="card-body">
-                        <h2 className="card-title">Skyhype Services</h2>
-                        <p>Online Service Providor App</p>
+                        <h2 className="card-title text-white">Skyhype Services</h2>
+                        <p className="text-white">Online Service Providor App</p>
                         <div className="card-actions justify-end">
-                            <button className="btn btn-primary"><a href="https://sky-hype-f2075.web.app/">View Project</a></button>
+                            <button className="btn btn-primary bg-red-600"><a href="https://sky-hype-f2075.web.app/">View Project</a></button>
                         </div>
                     </div>
                 </div>
-                <div className="card md:w-64 w-36 bg-base-100 shadow-xl">
+                <div className="card md:w-64 w-36 bg-black shadow-xl">
                     <figure><img src={P2} alt="Shoes" /></figure>
                     <div className="card-body">
-                        <h2 className="card-title">Gnosis Web App</h2>
-                        <p>Online EduTech</p>
+                        <h2 className="card-title text-white">Gnosis Web App</h2>
+                        <p className="text-white">Online EduTech</p>
                         <div className="card-actions justify-end">
-                            <button className="btn btn-primary"><a href="https://gnosis-web-app.web.app/">View Project</a></button>
+                            <button className="btn btn-primary bg-red-600"><a href="https://gnosis-web-app.web.app/">View Project</a></button>
                         </div>
                     </div>
                 </div>
-                <div className="card md:w-64 w-36 bg-base-100 shadow-xl">
+                <div className="card md:w-64 w-36 bg-black shadow-xl">
                     <figure><img src={P3} alt="Shoes" /></figure>
                     <div className="card-body">
-                        <h2 className="card-title">QuizQuark</h2>
-                        <p>Online Quiz Web App</p>
+                        <h2 className="card-title text-white">QuizQuark</h2>
+                        <p className="text-white">Online Quiz Web App</p>
                         <div className="card-actions justify-end">
-                            <button className="btn btn-primary"><a href="https://prismatic-starlight-792202.netlify.app/">View Project</a></button>
+                            <button className="btn btn-primary bg-red-600"><a href="https://prismatic-starlight-792202.netlify.app/">View Project</a></button>
                         </div>
                     </div>
                 </div>
